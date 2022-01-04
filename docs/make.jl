183deff022c067fa44a586f1b14e719ec0da600d
Jeroen van Zundert <mail@jeroenvanzundert.nl>
2022-01-04 10:39:36 +0100

    Upgrade docs build

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,15 @@
 using Documenter, FamaMacBeth
 
 makedocs(
-    format = :html,
     sitename = "FamaMacBeth.jl",
     modules = [FamaMacBeth],
     pages = ["index.md",
-            "api.md"
-             ]
+        "api.md"]
 )
 
 deploydocs(
     repo = "github.com/zundertj/FamaMacBeth.jl.git",
     target = "build",
-    julia  = "0.7",
-    deps   = nothing,
-    make   = nothing
+    deps = nothing,
+    make = nothing
 )
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,3 +1,10 @@
-# Tutorial
+# FamaMacBeth documentation
 
-TODO
+
+*FamaMacBeth.jl* is a Julia package that provides a simple implementation of Fama MacBeth, verified against the paper Petersen (2009).
+
+
+```@contents
+Pages = ["api.md"]
+Depth = 2
+```
