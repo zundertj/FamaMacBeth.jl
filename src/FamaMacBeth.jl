8113fe31086be64f0f6e9c900ed61222728c8ee8
Jeroen van Zundert <mail@jeroenvanzundert.nl>
2022-01-04 09:18:54 +0100

    Fix formatting

--- src/FamaMacBeth.jl
+++ src/FamaMacBeth.jl
@@ -19,20 +19,20 @@ mutable struct FMBModel <: StatsBase.RegressionModel
     X
     y
     g
-    resp::Union{FMBResp, Nothing}
+    resp::Union{FMBResp,Nothing}
 
-    function FMBModel(X,y,g,resp)
+    function FMBModel(X, y, g, resp)
         # checks on input
-        @assert size(X,1)==length(y) "y and X must have the same number of observations"
-        @assert length(y)==length(g) "g and y must have the same number of observations"
-        new(X,y,g,resp)
+        @assert size(X, 1) == length(y) "y and X must have the same number of observations"
+        @assert length(y) == length(g) "g and y must have the same number of observations"
+        new(X, y, g, resp)
     end
 end
 
 """
     FMBModel(X,y,g) -> FMBModel()
 """
-FMBModel(X,y,g) = FMBModel(X,y,g,nothing)
+FMBModel(X, y, g) = FMBModel(X, y, g, nothing)
 
 """
     group(m::FMBResp)
@@ -49,7 +49,7 @@ group(m::FMBModel) = group(m.resp)
 
 The estimated coefficients.
 """
-coef(m::FMBResp) = mean(m.coef, dims=2)
+coef(m::FMBResp) = mean(m.coef, dims = 2)
 coef(m::FMBModel) = coef(m.resp)
 
 """
@@ -58,7 +58,7 @@ coef(m::FMBModel) = coef(m.resp)
 
 The standard error around the estimated coefficients.
 """
-stderror(m::FMBResp) = std(m.coef, dims=2)./sqrt(size(m.coef,2))
+stderror(m::FMBResp) = std(m.coef, dims = 2) ./ sqrt(size(m.coef, 2))
 stderror(m::FMBModel) = stderror(m.resp)
 
 """
@@ -67,7 +67,7 @@ stderror(m::FMBModel) = stderror(m.resp)
 
 Variance-covariance matrix of the estimated coefficients.
 """
-vcov(m::FMBResp) = cov(m.coef, dims=2)./size(m.coef,2)
+vcov(m::FMBResp) = cov(m.coef, dims = 2) ./ size(m.coef, 2)
 vcov(m::FMBModel) = vcov(m.resp)
 
 """
@@ -76,7 +76,7 @@ vcov(m::FMBModel) = vcov(m.resp)
 
 The t-statistic per coefficient whether the coefficient equals zero or not.
 """
-tstat(m::FMBResp) = coef(m)./stderror(m)
+tstat(m::FMBResp) = coef(m) ./ stderror(m)
 tstat(m::FMBModel) = tstat(m.resp)
 
 """
@@ -107,9 +107,9 @@ residuals(m::FMBModel) = m.y .- predict(m)
 The un-adjusted R2.
 """
 function r2(m::FMBModel)
-    SSE = sum(residuals(m).^2)
-    SST = sum((m.y .- mean(m.y)).^2)
-    1-SSE/SST
+    SSE = sum(residuals(m) .^ 2)
+    SST = sum((m.y .- mean(m.y)) .^ 2)
+    1 - SSE / SST
 end
 
 """
@@ -119,8 +119,8 @@ The adjusted R2.
 """
 function adjr2(m::FMBModel)
     n = length(m.y)
-    p = size(m.X,2)
-    1.0-(1.0-r2(m))*(n-1)/(n-p)
+    p = size(m.X, 2)
+    1.0 - (1.0 - r2(m)) * (n - 1) / (n - p)
 end
 
 """
@@ -128,7 +128,7 @@ end
 
 Short-hand for fit(FMBModel(X,y,g))
 """
-fmb(X::Array, y, g) = fit(FMBModel(X,y,g))
+fmb(X::Array, y, g) = fit(FMBModel(X, y, g))
 
 """
     fit(m::FMBModel) -> FMBModel()
@@ -140,13 +140,13 @@ using the resulting coefficients. An intercept is not added to `X`.
 function fit(m::FMBModel)
     # pre-allocate output
     ug = unique(m.g)
-    coef = zeros(eltype(m.X), size(m.X,2), length(ug))
+    coef = zeros(eltype(m.X), size(m.X, 2), length(ug))
     nobs = zeros(Int64, length(ug))
 
     # per group: OLS
     for (i, gs) in enumerate(ug)
-        select = m.g.==gs
-        coef[:,i] = m.X[select,:]\m.y[select]
+        select = m.g .== gs
+        coef[:, i] = m.X[select, :] \ m.y[select]
         nobs[i] = sum(select)
     end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,24 +5,24 @@ using ReadStat, DataValues # for the Petersen data set
 using FamaMacBeth
 
 @testset "general" begin
-    y = rand(100,1)
-    X = rand(100,3)
+    y = rand(100, 1)
+    X = rand(100, 3)
 
     # if all in the same group, we should get OLS coefficients
-    g = vec(ones(size(y,1),1))
-    @test coef(fmb(X,y,g))==X\y
+    g = vec(ones(size(y, 1), 1))
+    @test coef(fmb(X, y, g)) == X \ y
 
-    g2 = repeat(collect(1:10),inner=10)
-    resp = fmb(X,y,g2)
-    @test all(nobs(resp).==10)
-    @test size(coef(resp),1) == 3
-    @test size(coef(resp),2) == 1 # because we take the mean!
+    g2 = repeat(collect(1:10), inner = 10)
+    resp = fmb(X, y, g2)
+    @test all(nobs(resp) .== 10)
+    @test size(coef(resp), 1) == 3
+    @test size(coef(resp), 2) == 1 # because we take the mean!
     @test group(resp) == collect(1:10)
     @test size(vcov(resp)) == (3, 3)
 end
 
 @testset "petersen_dataset" begin
-    # download and prcess data
+    # download and process data
     file = "https://www.kellogg.northwestern.edu/faculty/petersen/htm/papers/se/test_data.dta"
     loc = download(file)
     data = read_dta(loc)
@@ -32,12 +32,12 @@ end
     y = Float64.(get.(data.data[4]))
 
     # fit
-    m = fmb([ones(size(x,1),1) x], y, year)
+    m = fmb([ones(size(x, 1), 1) x], y, year)
 
     # compare with https://www.kellogg.northwestern.edu/faculty/petersen/htm/papers/se/test_data.htm
-    @test all(round.(coef(m), digits=4) .== [.0313, 1.0356])
-    @test all(round.(stderror(m), digits=4) .== [.0234, .0333])
-    @test all(round.(tstat(m), digits=2) .== [1.34, 31.06])
+    @test all(round.(coef(m), digits = 4) .== [0.0313, 1.0356])
+    @test all(round.(stderror(m), digits = 4) .== [0.0234, 0.0333])
+    @test all(round.(tstat(m), digits = 2) .== [1.34, 31.06])
     # @test round(adjr2(m),4)==.2078 # this is what Petersen lists, but cannot reproduce
-    @test round(adjr2(m);digits=4)==.2076
+    @test round(adjr2(m); digits = 4) == 0.2076
 end
